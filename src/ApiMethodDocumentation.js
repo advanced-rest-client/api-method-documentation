a18b8b46333de166d13cbaeba07a83f7a920a707
leandrogilcarrano <leandro.gil@mulesoft.com>
2024-04-22 16:13:33 -0300

    feat(W-15520261): show  example when is asyncApi

--- src/ApiMethodDocumentation.js
+++ src/ApiMethodDocumentation.js
@@ -991,14 +991,17 @@ export class ApiMethodDocumentation extends AmfHelperMixin(LitElement) {
       compatibility,
       graph
     } = this;
-    const isAsyncApi = !this._isAsyncAPI(amf)
+
+    // hidden example when is not an async api 
+    const noMainExample = !this._isAsyncAPI(amf)
+
     return html`<api-headers-document
       opened
       .amf="${amf}"
       ?narrow="${narrow}"
       ?compatibility="${compatibility}"
       ?graph="${graph}"
-      .noMainExample="${isAsyncApi}"
+      .noMainExample="${noMainExample}"
       .headers="${headers}"></api-headers-document>`;
   }
 
